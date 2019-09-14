c8265ddcc86c2ef4dff2515cf085261e01e62f1b
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-14 01:40:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,7 +31,8 @@ def items
 end
   # @total -= @last_transaction_amount
 def void_last_transaction()
-  self.total -= self.last_transaction_amount 
+ # self.total -= self.last_transaction_amount 
+ self.last_transaction_amount -= self.total
 end
 end
 
